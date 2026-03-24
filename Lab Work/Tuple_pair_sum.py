524b1c912451393624ca8ad74349332be5844ca8
Drishty Garg <drishty0401@gmail.com>
2026-03-24 09:34:18 +0530

    made 15 programs related to list,tuple and set

---
+++ Lab Work/Tuple_pair_sum.py
@@ -0,0 +1,8 @@
+'''Question 10: Tuple Pair Sum
+Problem Statement:
+Find all pairs in a tuple whose sum equals a given value.
+Input:
+Tuple = (1, 2, 3, 4, 5), Sum = 5
+Output:
+[(1, 4), (2, 3)'''
+
